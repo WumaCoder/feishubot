a28661012e15d6cf7092991f0c6903789c14bfcc
WumaCoder <15804854160@163.com>
2023-07-19 19:46:01 +0800

    fix: 测试发版

--- src/greet.test.ts
+++ src/greet.test.ts
@@ -5,7 +5,7 @@ import { greet } from "./greet.js";
 const message = "Yay, testing!";
 
 describe("greet", () => {
-	it("logs to the console once when message is provided as a string", () => {
+	it("logs to the console once when message is provided as a string2", () => {
 		const logger = vi.spyOn(console, "log").mockImplementation(() => undefined);
 
 		greet(message);
