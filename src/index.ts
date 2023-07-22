e02503489f7da2f4eaacdc6c4606cedd3ce5b522
WumaCoder <15804854160@163.com>
2023-07-22 13:15:30 +0800

    fix: 更新版本号

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import serveCmd from "./serve.js";
 
 const program = new commander.Command("feishu bot");
 
-program.version("0.0.11");
+program.version("0.0.20");
 
 const interactive = program.command("interactive");
 interactiveCmd(interactive);
